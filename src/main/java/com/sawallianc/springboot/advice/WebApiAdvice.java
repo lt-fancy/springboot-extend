d6882a1fbeff83b8cf89eb82de06987cf84477ca
fingertap <fingertap588@gmail.com>
2018-11-21 16:06:19 +0800

    优化错误日志

--- src/main/java/com/sawallianc/springboot/advice/WebApiAdvice.java
+++ src/main/java/com/sawallianc/springboot/advice/WebApiAdvice.java
@@ -1,5 +1,6 @@
 package com.sawallianc.springboot.advice;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sawallianc.entity.Result;
 import com.sawallianc.entity.ResultCode;
 import com.sawallianc.entity.exception.BizRuntimeException;
@@ -16,18 +17,17 @@ public class WebApiAdvice {
     }
 
     @ExceptionHandler
-    public Object handleExceotion(Exception e){
+    public Object handleException(Exception e){
         Result result = Result.failResult();
-        String errMsg;
         if(e instanceof BizRuntimeException){
             ResultCode resultCode = ((BizRuntimeException) e).getResultCode();
             result.setResultCode(resultCode);
             result.setMsg(resultCode.getMsg());
-            errMsg = "[error code: "+resultCode.getCode()+"]";
         } else {
-            errMsg = "[error code: -1]";
+            result.setMsg(e.toString());
+            result.setCode(-1);
         }
-        LOGGER.error(errMsg,e);
+        LOGGER.error(JSONObject.toJSONString(result));
         return result;
     }
 }
